3daad43cab358b69293208dcb434727a113069f8
Christopher Bech-Jensen <bechjensen@ruc.dk>
2024-10-22 15:25:32 +0200

    Add project files.

---
+++ DataLayer/DataService.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer;
+public class DataService : IDataService
+{
+    public int AddCategory(string name, string description)
+    {
+        var db = new NorthwindContext();
+
+        int id = db.Categories.Max(x => x.Id) + 1;
+        var category = new Category
+        {
+            Id = id,
+            Name = name,
+            Description = description
+        };
+
+        db.Categories.Add(category);
+
+        db.SaveChanges();
+
+        return category.Id;
+
+    }
+
+    public bool UpdateCategory(int id, string name, string description)
+    {
+        var db = new NorthwindContext();
+
+        var category = db.Categories.Find(id);
+
+        if (category == null)
+        {
+            return false;
+        }
+
+        category.Name = name;
+        category.Description = description;
+
+        return db.SaveChanges() > 0;
+
+    }
+
+    public bool UpdateCategory(Category category)
+    {
+        var db = new NorthwindContext();
+
+        db.Update(category);
+        //db.Entry(category).State = EntityState.Modified;
+
+
+        return db.SaveChanges() > 0;
+
+
+    }
+
+    public bool DeleteCategory(int id)
+    {
+        var db = new NorthwindContext();
+
+        var category = db.Categories.Find(id);
+
+        if (category == null)
+        {
+            return false;
+        }
+
+        db.Categories.Remove(category);
+
+        return db.SaveChanges() > 0;
+
+    }
+
+    public IList<Category> GetCategories()
+    {
+        var db = new NorthwindContext();
+        return db.Categories.ToList();
+    }
+
+    public IList<Product> GetProducts()
+    {
+        var db = new NorthwindContext();
+        return db.Products.Include(x => x.Category).ToList();
+    }
+}
+
---
+++ DbApp/DbAppProgram.cs
@@ -0,0 +1,41 @@
+﻿using DataLayer;
+
+var dataservice = new DataService();
+
+//var categories = dataservice.GetCategories();
+
+PrintCategories(dataservice);
+
+//dataservice.AddCategory("dsfsfksdlfk", "sfæsjdoi");
+//dataservice.DeleteCategory(9999);
+
+var category = new Category
+{
+    Id = 9,
+    Name = "123",
+    Description = "456"
+};
+
+dataservice.UpdateCategory(category);
+
+PrintCategories(dataservice);
+
+//PrintProducts(dataservice);
+
+
+
+static void PrintProducts(IDataService dataService)
+{
+    foreach (var e in dataService.GetProducts())
+    {
+        Console.WriteLine($"{e.Id}, {e.Name}, {e.Category.Name}");
+    }
+}
+
+static void PrintCategories(IDataService dataService)
+{
+    foreach (var e in dataService.GetCategories())
+    {
+        Console.WriteLine($"{e.Id}, {e.Name}, {e.Description}");
+    }
+}
